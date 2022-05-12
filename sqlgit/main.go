f7b226f456e240ce0a1762ff6e45446c32edeef2
Mahmud <mahmud4563253@gmail.com>
2022-05-12 19:47:37 +0500

    psql

--- sqlgit/main.go
+++ sqlgit/main.go
@@ -1,9 +1,28 @@
 package main
 
-import "database/sql"
+import (
+	"database/sql"
+	"fmt"
+	_"github.com/lib/pq"
+)
 
 func main(){
-	connStr:="user=postgres password=1 dbname=fruitdb sslmode=disable"
+	connStr:="user=postgres password=1 dbname=testdb sslmode=disable"
 	db,err:=sql.Open("postgres",connStr)
 	
+	if err!=nil{
+		panic("error while connecting to db")
+	}
+	defer db.Close()
+
+
+	query:= `INSERT INTO users (username, pasword, email) values($1,$2,$3)`
+	result,err:=db.Exec(query,"Mahmud","mahmud3253","mahmud4563253@gmail.com")
+	if err!=nil{
+		fmt.Println(err)
+		return
+	}
+
+	fmt.Println(result.LastInsertId())
+	fmt.Println(result.RowsAffected())
 }
